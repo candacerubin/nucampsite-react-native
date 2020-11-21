8f363924a320dfe3c6d873719500e6cde7de6346
candacerubin <candacerubin0@gmail.com>
2020-11-21 10:42:18 -0800

    About and Contact pages

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
 const DirectoryNavigator = createStackNavigator(
 	{
@@ -24,7 +28,71 @@ const DirectoryNavigator = createStackNavigator(
 	}
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+	{
+		Home: { screen: Home },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				color: '#fff',
+			},
+		},
+	}
+);
+
+const AboutNavigator = createStackNavigator(
+	{
+		About: { screen: About },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				color: '#fff',
+			},
+		},
+	}
+);
+
+const ContactNavigator = createStackNavigator(
+	{
+		Contact: { screen: Contact },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				color: '#fff',
+			},
+		},
+	}
+);
+
+const MainNavigator = createDrawerNavigator(
+	{
+		Home: { screen: HomeNavigator },
+		Directory: { screen: DirectoryNavigator },
+		About: { screen: AboutNavigator },
+		Contact: { screen: ContactNavigator },
+	},
+	{
+		drawerBackgroundColor: '#CEC8FF',
+	}
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
+
 class Main extends Component {
 	render() {
 		return (

2e030a3d7878eed34f474af94e17a81083278e1f
candacerubin <candacerubin0@gmail.com>
2020-12-03 02:27:41 -0800

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { ScrollView } from 'react-native-gesture-handler';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 export default class Contact extends Component {
 	static navigationOptions = {
@@ -9,15 +10,17 @@ export default class Contact extends Component {
 	};
 	render() {
 		return (
-			<ScrollView>
-				<Card title='Contact Information' wrapperStyle={{ margin: 20 }}>
-					<Text>1 Nucamp Way</Text>
-					<Text>Seattle, WA 98001</Text>
-					<Text style={{ marginBottom: 10 }}>U.S.A.</Text>
-					<Text>Phone: 1-206-555-1234</Text>
-					<Text>Email: campsites@nucamp.co</Text>
-				</Card>
-			</ScrollView>
+			<Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+				<ScrollView>
+					<Card title='Contact Information' wrapperStyle={{ margin: 20 }}>
+						<Text>1 Nucamp Way</Text>
+						<Text>Seattle, WA 98001</Text>
+						<Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+						<Text>Phone: 1-206-555-1234</Text>
+						<Text>Email: campsites@nucamp.co</Text>
+					</Card>
+				</ScrollView>
+			</Animatable.View>
 		);
 	}
 }

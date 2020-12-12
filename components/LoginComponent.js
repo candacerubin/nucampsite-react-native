e28959a54b553f4fd05c3f44e9ad4256068d42be
candacerubin <candacerubin0@gmail.com>
2020-12-12 11:58:10 -0800

    RN Week 4 Workshop 2

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -154,6 +154,21 @@ class RegisterTab extends Component {
 		}
 	};
 
+	getImageFromGallery = async () => {
+		const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+		if (cameraRollPermission.status === 'granted') {
+			const capturedImage = await ImagePicker.launchImageLibraryAsync({
+				allowsEditing: true,
+				aspect: [1, 1],
+			});
+			if (!capturedImage.cancelled) {
+				console.log(capturedImage);
+				this.processImage(capturedImage.uri);
+			}
+		}
+	};
+
 	processImage = async (imgUri) => {
 		const processedImage = await ImageManipulator.manipulateAsync(
 			imgUri,
@@ -189,6 +204,7 @@ class RegisterTab extends Component {
 							style={styles.image}
 						/>
 						<Button title='Camera' onPress={this.getImageFromCamera} />
+						<Button title='Gallery' onPress={this.getImageFromGallery} />
 					</View>
 					<Input
 						placeholder='Username'
